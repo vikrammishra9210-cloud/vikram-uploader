924e981e0aeece11f630799665378a6c30bc9680
vikrammishra9210-cloud <vikrammishra9210@gmail.com>
2025-11-03 18:14:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,6 +2,6 @@
 import os
 
 # You can keep these empty and set environment variables in Render instead.
-API_ID = int(os.getenv('API_ID') or 0)
-API_HASH = os.getenv('API_HASH') or ''
-BOT_TOKEN = os.getenv('BOT_TOKEN') or ''
+API_ID = int(os.getenv('23698677') or 0)
+API_HASH = os.getenv('001400def80e870e3eef60bb38d8b871') or ''
+BOT_TOKEN = os.getenv('7496096578:AAFBDcRA2k0XdUcBWcbawOM1Q0pFBsdPyx0') or ''
